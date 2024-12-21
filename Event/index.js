1b095730cd6a794aa6158e9aa4c692684cb5307e
durrealiyeva <durrealiyeva0@gmail.com>
2024-12-21 16:11:16 +0400

    keeping

---
+++ Event/index.js
@@ -0,0 +1,42 @@
+//? Task1
+// const input=document.querySelector("#inputField")
+// const text=document.querySelector("#output")
+// input.addEventListener("keydown",(e)=>{
+//    text.textContent=e.key
+// })
+
+//? Task2
+// document.querySelector("#focusInput").addEventListener("focus",function(){
+//     this.style.borderColor="green"
+// })
+
+//? Task3
+// const input=document.querySelector("#focusInput")
+// input.addEventListener("focus",function(){
+//     this.style.borderColor="green"
+// })
+// input.addEventListener("blur",function(){
+//     this.style.borderColor="black"
+// })
+
+//? Task4
+// document.querySelector("#myForm").addEventListener("submit",function(e){
+//     e.preventDefault()
+//     alert("Form gonderilmedi")
+// })
+
+//? Task5
+// function displayWindowSize(){
+//   const w=window.outerWidth
+//   const h=window.outerHeight
+//   document.querySelector("#sizeInfo").innerHTML=`Width=${w},Height=${h}`
+// }
+// window.addEventListener("resize",displayWindowSize)
+// displayWindowSize()
+
+//? Task6
+// window.addEventListener("scroll",function(){
+//     const x=window.scrollX
+//     const y=window.scrollY
+//     document.querySelector("#scrollInfo").innerHTML=`Ufuqi=${x}px,Saquli=${y}px`
+// })
